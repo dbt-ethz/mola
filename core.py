f365fe896b3651a87ef359486c79863e3aed8e2c
modellstadt <contact@benjamin-dillenburger.com>
2019-02-23 11:59:32 +0100

    Update core.py
    def to class

--- core.py
+++ core.py
@@ -1,20 +1,20 @@
-def Vertex:
+class Vertex:
     def __init__(x=0,y=0,z=0):
         self.x=x
         self.y=y
         self.z=z
 
-def Face:
+class Face:
     def __init__(self,vertices=[]):
         self.vertices = vertices
         self.color=(1,1,1,1)
 
-def Edge:
+class Edge:
     def __init__(v1,v2):
         self.v1=v1
         self.v2=v2
 
-def Mesh:
+class Mesh:
     def __init__(vertices=[],faces=[],edges=[]):
         self.vertices=vertices
         self.faces=faces
