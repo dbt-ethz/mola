bdab777102d7a5b43305979f3a350550cae9ffe4
modellstadt <contact@benjamin-dillenburger.com>
2019-02-23 20:12:19 +0100

    Update meshFactory.py

--- meshFactory.py
+++ meshFactory.py
@@ -1,7 +1,6 @@
-from mola.core import Mesh 
-from mola.core import Vertex 
-from mola.core import Face
-
+from core import Mesh 
+from core import Vertex 
+from core import Face
 def constructBoxMesh(x1,y1,z1,x2,y2,z2):
     mesh=Mesh()
     v1 = Vertex(x1,y1,z1)
