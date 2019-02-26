1b2e7c394a638f2f67ff3bd3ef08568a943ae8a4
demsham <desettled@gmail.com>
2019-02-26 19:51:04 +0200

    Update meshAnalysis.py
    private methods

--- meshAnalysis.py
+++ meshAnalysis.py
@@ -1,20 +1,20 @@
 import math
 import mola.meshMath as _vec
 
-def determinant(a,b,c,d,e,f,g,h,i):
+def __determinant(a,b,c,d,e,f,g,h,i):
 	return (a * e * i - a * f * h - b * d * i + b * f * g + c * d * h - c * e * g)
 
-def getTriangleArea3D(a,b,c):
-	return getTriangleArea3DCoords(a.x,a.y,a.z,b.x,b.y,b.z,c.x,c.y,c.z)
+def __getTriangleArea3D(a,b,c):
+	return __getTriangleArea3DCoords(a.x,a.y,a.z,b.x,b.y,b.z,c.x,c.y,c.z)
 
-def getTriangleArea3DCoords(xa,ya,za,xb,yb,zb,xc,yc,zc):
-	return 0.5 * math.sqrt(math.pow(determinant(xa, xb, xc, ya, yb, yc, 1, 1, 1), 2) + math.pow(determinant(ya, yb, yc, za, zb, zc, 1, 1, 1), 2) + math.pow(determinant(za, zb, zc, xa, xb, xc, 1, 1, 1), 2))
+def __getTriangleArea3DCoords(xa,ya,za,xb,yb,zb,xc,yc,zc):
+	return 0.5 * math.sqrt(math.pow(__determinant(xa, xb, xc, ya, yb, yc, 1, 1, 1), 2) + math.pow(__determinant(ya, yb, yc, za, zb, zc, 1, 1, 1), 2) + math.pow(__determinant(za, zb, zc, xa, xb, xc, 1, 1, 1), 2))
 
 def getFaceArea(face):
 	if(len(face.vertices) == 3):
-		return getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2])
+		return __getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2])
 	else:
-		return getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2]) + getTriangleArea3D(face.vertices[1],face.vertices[2],face.vertices[3])
+		return __getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2]) + __getTriangleArea3D(face.vertices[1],face.vertices[2],face.vertices[3])
 
 def getFacePerimeter(face):
 	sum = 0;
