af98192ec3d4ce609c01b20b4b660b187b652c2a
demsham <desettled@gmail.com>
2019-02-26 19:36:51 +0200

    Update meshColorizeFaces.py
    moved analysis to separate py file + changed methods to private

--- meshColorizeFaces.py
+++ meshColorizeFaces.py
@@ -1,6 +1,6 @@
-import math
 import colorsys
 import mola.meshMath as vec
+import mola.meshAnalysis as analysis
 
 def __getColorRgb(hue):
 	col = colorsys.hsv_to_rgb(hue,1,1)
@@ -11,67 +11,41 @@ def mapValuesToColor(values):
 	valueMax = max(values)
 	colors=[]
 	for v in values:
-		h = map(v,valueMin,valueMax,0.0,1.0)
+		h = __map(v,valueMin,valueMax,0.0,1.0)
 		colors.append(__getColorRgb(h))
 	return colors
 
 def colorFacesByArea(faces):
 	values = []
 	for face in faces:
-		values.append(getFaceArea(face))
+		values.append(analysis.getFaceArea(face))
 	valueMin = min(values)
 	valueMax = max(values)
 	for i, face in enumerate(faces):
-		h = map(values[i],valueMin,valueMax,0.0,1.0)
+		h = __map(values[i],valueMin,valueMax,0.0,1.0)
 		face.color = __getColorRgb(h)
 
 def colorFacesByPerimeter(faces):
 	values = []
 	for face in faces:
-		values.append(getFacePerimeter(face))
+		values.append(analysis.getFacePerimeter(face))
 	valueMin = min(values)
 	valueMax = max(values)
 	for i , face in enumerate(faces):
-		h = map(values[i],valueMin,valueMax,0.0,1.0)
+		h = __map(values[i],valueMin,valueMax,0.0,1.0)
 		face.color = __getColorRgb(h)
 
 def colorFacesByVerticality(faces):
 	values = []
 	for face in faces:
-		values.append(getFaceVerticality(face))
+		values.append(analysis.getFaceVerticality(face))
 	valueMin = min(values)
 	valueMax = max(values)
 	for i , face in enumerate(faces):
-		h = map(values[i],valueMin,valueMax,0.0,1.0)
+		h = __map(values[i],valueMin,valueMax,0.0,1.0)
 		face.color = __getColorRgb(h)
 
-def determinant(a,b,c,d,e,f,g,h,i):
-	return (a * e * i - a * f * h - b * d * i + b * f * g + c * d * h - c * e * g)
-
-def getTriangleArea3D(a,b,c):
-	return getTriangleArea3DCoords(a.x,a.y,a.z,b.x,b.y,b.z,c.x,c.y,c.z)
-	#return getTriangleArea3DCoords(a[0],a[1],a[2],b[0],b[1],b[2],c[0],c[1],c[2])
-
-def getTriangleArea3DCoords(xa,ya,za,xb,yb,zb,xc,yc,zc):
-	return 0.5 * math.sqrt(math.pow(determinant(xa, xb, xc, ya, yb, yc, 1, 1, 1), 2) + math.pow(determinant(ya, yb, yc, za, zb, zc, 1, 1, 1), 2) + math.pow(determinant(za, zb, zc, xa, xb, xc, 1, 1, 1), 2))
-
-def map(value, fromMin, fromMax, toMin, toMax):
+def __map(value, fromMin, fromMax, toMin, toMax):
 	return toMin + ((toMax - toMin) / (fromMax - fromMin)) * (value - fromMin)
 
-def getFaceArea(face):
-	if(len(face.vertices) == 3):
-		return getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2])
-	else:
-		return getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2]) + getTriangleArea3D(face.vertices[1],face.vertices[2],face.vertices[3])
 
-def getFacePerimeter(face):
-	sum = 0;
-	for i in range(len(face.vertices)):
-		v1 = face.vertices[i]
-		v2 = face.vertices[(i+1)%len(face.vertices)]
-		sum += vec.VectorDistance(v1,v2)
-	return sum
-
-def getFaceVerticality(face):
-	normal = vec.VectorNormalFromVertices(face.vertices)
-	return math.atan2(normal[1] * normal[1], normal[0] * normal[0])
