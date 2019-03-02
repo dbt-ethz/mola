42c7a01cd46b64e8f13d3b3c7155bc4fcb9873cf
modellstadt <contact@benjamin-dillenburger.com>
2019-03-02 09:21:24 +0100

    Update and rename meshDisplayProcessing to meshDisplayProcessing.py

---
+++ meshDisplayProcessing.py
@@ -0,0 +1,17 @@
+def displayLines(lines):
+  for l in lines:
+    line(l.v1.x,l.v1.y,l.v2.x,l.v2.y)
+    
+def display(faces):
+  for f in faces:
+    beginShape()
+    for v in f.vertices:
+      vertex(v.x,v.y,v.z)
+    endShape(CLOSE)
+
+def display2D(faces):
+  for f in faces:
+    beginShape()
+    for v in f.vertices:
+      vertex(v.x,v.y)
+    endShape(CLOSE)
