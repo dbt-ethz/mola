d908a88df2cadfc5a377199e4aacc90bc9277522
modellstadt <contact@benjamin-dillenburger.com>
2019-03-03 19:51:43 +0100

    Update core.py

--- core.py
+++ core.py
@@ -19,10 +19,12 @@ class Edge:
         self.v2=v2
         self.face1=None
         self.face2=None
+        
     def getCenter(self):
         return Vertex((self.v2.x+self.v1.x)/2.0,(self.v2.y+self.v1.y)/2.0,(self.v2.z+self.v1.z)/2.0)
 
 class Box:
+    
     def __init__(self,x1=-float('inf'),y1=-float('inf'),z1=-float('inf'),x2=float('inf'),y2=float('inf'),z2=float('inf')):
         self.x1=x1
         self.y1=y1
@@ -30,18 +32,25 @@ class Box:
         self.x2=x2
         self.y2=y2
         self.z2=z2
+        
     def getDimX(self):
         return self.x2-self.x1
+    
     def getDimY(self):
         return self.y2-self.y1
+    
     def getDimZ(self):
         return self.z2-self.z1
+    
     def getCenterX(self):
         return (self.x2+self.x1)/2
+    
     def getCenterY(self):
         return (self.y2-self.y1)/2
+    
     def getCenterZ(self):
         return (self.z2-self.z1)/2
+    
     def addPoint(self,x,y,z):
         self.x1=min(x,self.x1)
         self.y1=min(y,self.y1)
