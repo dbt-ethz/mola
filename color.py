11046bcbea04dbdd6de69d490398db27123f7ba2
modellstadt <contact@benjamin-dillenburger.com>
2019-03-04 02:04:20 +0100

    Update color.py
    use of colorFacesByFunction to shorten

--- color.py
+++ color.py
@@ -1,5 +1,5 @@
-import colorsys as _colorsys
-import mola.analysis as _analysis
+import colorsys as colorsys
+import mola.analysis as analysis
 
 __grayscale = False;
 
@@ -8,17 +8,8 @@ def __getColorRgb(hue):
 		return (hue,hue,hue,1)
 	else:
 		hue = __map(hue,0.0,1.0,0.0,0.8) #limit hue red-red to red-magenta
-		col = _colorsys.hsv_to_rgb(hue,1,1)
+		col = colorsys.hsv_to_rgb(hue,1,1)
 		return (col[0],col[1],col[2],1)
-
-def mapValuesToColor(values):
-	valueMin = min(values)
-	valueMax = max(values)
-	colors=[]
-	for v in values:
-		h = __map(v,valueMin,valueMax,0.0,1.0)
-		colors.append(__getColorRgb(h))
-	return colors
 
 def colorFacesByFunction(faces,faceFunction):
 	"""
@@ -39,28 +30,14 @@ def colorFacesByArea(faces):
 	Assigns a color to all the faces by area,
 	from smallest (red) to biggest (purple).
 	"""
-	values = []
-	for face in faces:
-		values.append(_analysis.getFaceArea(face))
-	valueMin = min(values)
-	valueMax = max(values)
-	for i, face in enumerate(faces):
-		h = __map(values[i],valueMin,valueMax,0.0,1.0)
-		face.color = __getColorRgb(h)
+	colorFacesByFunction(faces,analysis.getFaceArea)
 
 def colorFacesByPerimeter(faces):
 	"""
 	Assigns a color to all the faces by perimeter,
 	from smallest (red) to biggest (purple).
 	"""
-	values = []
-	for face in faces:
-		values.append(_analysis.getFacePerimeter(face))
-	valueMin = min(values)
-	valueMax = max(values)
-	for i , face in enumerate(faces):
-		h = __map(values[i],valueMin,valueMax,0.0,1.0)
-		face.color = __getColorRgb(h)
+	colorFacesByFunction(faces,analysis.getFacePerimeter)
 
 def colorFacesByCompactness(faces):
 	"""
@@ -83,14 +60,7 @@ def colorFacesByVerticality(faces):
 	Assigns a color to all the faces by verticality,
 	from smallest (red) to biggest (purple).
 	"""
-	values = []
-	for face in faces:
-		values.append(_analysis.getFaceVerticality(face))
-	valueMin = min(values)
-	valueMax = max(values)
-	for i , face in enumerate(faces):
-		h = __map(values[i],valueMin,valueMax,0.0,1.0)
-		face.color = __getColorRgb(h)
+	colorFacesByFunction(faces,analysis.getFaceVerticality)
 
 def __map(value, fromMin, fromMax, toMin, toMax):
 	return toMin + ((toMax - toMin) / (fromMax - fromMin)) * (value - fromMin)
