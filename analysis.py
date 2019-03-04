485931eb99f059141cc81858f9cae7108be8cf73
worbit <bernhard@arch.ethz.ch>
2019-03-04 11:22:11 +0100

    tabs and spaces

--- analysis.py
+++ analysis.py
@@ -11,31 +11,39 @@ def __getTriangleArea3DCoords(xa,ya,za,xb,yb,zb,xc,yc,zc):
 	return 0.5 * _math.sqrt(_math.pow(__determinant(xa, xb, xc, ya, yb, yc, 1, 1, 1), 2) + _math.pow(__determinant(ya, yb, yc, za, zb, zc, 1, 1, 1), 2) + _math.pow(__determinant(za, zb, zc, xa, xb, xc, 1, 1, 1), 2))
 
 def getFaceCompactness(face):
+	"""
+	Returns the compactness of a face as the ratio between area and perimeter.
+
+	Arguments:
+	----------
+	face : mola.core.Face
+			The face to be measured
+	"""
 	return analysis.getFaceArea(face)/analysis.getFacePerimeter(face)
 
 def getFaceArea(face):
-    """
-    Returns the area of a face, for quads that of two triangles.
+	"""
+	Returns the area of a face, for quads that of two triangles.
 
-    Arguments:
-    ----------
-    face : mola.core.Face
-        The face to be measured
-    """
+	Arguments:
+	----------
+	face : mola.core.Face
+			The face to be measured
+	"""
 	if(len(face.vertices) == 3):
 		return __getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2])
 	else:
 		return __getTriangleArea3D(face.vertices[0],face.vertices[1],face.vertices[2]) + __getTriangleArea3D(face.vertices[2],face.vertices[3],face.vertices[0])
 
 def getFacePerimeter(face):
-    """
-    Returns the perimeter of a face as the sum of all the edges' lengths.
+	"""
+	Returns the perimeter of a face as the sum of all the edges' lengths.
 
-    Arguments:
-    ----------
-    face : mola.core.Face
-        The face to be measured
-    """
+	Arguments:
+	----------
+	face : mola.core.Face
+			The face to be measured
+	"""
 	sum = 0;
 	for i in range(len(face.vertices)):
 		v1 = face.vertices[i]
@@ -44,14 +52,14 @@ def getFacePerimeter(face):
 	return sum
 
 def getFaceVerticality(face):
-    """
-    Returns the verticality of a face as the angle between ??.
+	"""
+	Returns the verticality of a face as the angle between ??.
 
-    Arguments:
-    ----------
-    face : mola.core.Face
-        The face to be measured
-    """
+	Arguments:
+	----------
+	face : mola.core.Face
+			The face to be measured
+	"""
 	normal = _vec.VectorNormalFromVertices(face.vertices)
 	return _math.atan2(normal.y * normal.y, normal.x * normal.x)
 	#return _math.atan2(normal[1] * normal[1], normal[0] * normal[0])
