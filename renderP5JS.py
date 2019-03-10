3117a076dca7b5eebcfb7c104d9d8b00af9a0d4e
modellstadt <contact@benjamin-dillenburger.com>
2019-03-10 09:18:54 +0100

    procedural drawing methods

--- renderP5JS.py
+++ renderP5JS.py
@@ -26,38 +26,50 @@ def _endDraw2D():
     return '''</script>'''
 
 def noStroke():
+    global p5code
     p5code+="noStroke();"
 
 def noFill():
+    global p5code
     p5code+="noFill();"
 
 def strokeWeight(weight):
+    global p5code
     p5code+="strokeWeight("+str(weight)+");"
 
 def stroke(r,g,b):
+    global p5code
     p5code+="stroke("+str(r)+","+str(g)+","+str(b)+");"
 
 def fill(r,g,b):
+    global p5code
     p5code+="fill("+str(r)+","+str(g)+","+str(b)+");"
 
 def line(x1,y1,x2,y2):
+    global p5code
     p5code+="line("+str(x1)+","+str(y1)+","+str(x2)+","+str(y2)+");"
 
 def rect(x1,y1,x2,y2):
+    global p5code
     p5code+="rect("+str(x1)+","+str(y1)+","+str(x2)+","+str(y2)+");"
 
 def beginShape():
+    global p5code
     p5code+="beginShape();"
 
 def endShape():
+    global p5code
     p5code+="endShape();"
 
 def vertex(x,y):
+    global p5code
     p5code+="vertex("+str(x)+","+str(y)+");"
 
 def beginDraw():
+    global p5code
     p5code=_beginDraw2D()
 
 def endDraw():
+    global p5code
     p5code+=_endDraw2D()
     return p5code
