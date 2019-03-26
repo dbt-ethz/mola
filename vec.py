6e000c2fef179f05fe5de66548cce31739ee717d
worbit <bernhard@arch.ethz.ch>
2019-03-26 11:45:00 +0100

    .. to ....

--- vec.py
+++ vec.py
@@ -42,13 +42,13 @@ def cross(v1,v2):
     return _Vertex(v1.y * v2.z - v2.y * v1.z, v1.z * v2.x - v2.z * v1.x, v1.x * v2.y - v2.x * v1.y)
 
 def dot(v1,v2):
-  return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
 
 def distance(v1,v2):
-  dX=v2.x-v1.x
-  dY=v2.y-v1.y
-  dZ=v2.z-v1.z
-  return _math.sqrt(dX*dX+dY*dY+dZ*dZ)
+    dX=v2.x-v1.x
+    dY=v2.y-v1.y
+    dZ=v2.z-v1.z
+    return _math.sqrt(dX*dX+dY*dY+dZ*dZ)
 
 def center(v1,v2):
     return _Vertex((v1.x+v2.x)/2,(v1.y+v2.y)/2,(v1.z+v2.z)/2)
@@ -61,33 +61,33 @@ def betweenAbs( v1,  v2,  f):
     return betweenRel(v1, v2, f / d)
 
 def lineLineIntersection(a,b,c,d):
-  deltaABX=b.x - a.x
-  deltaABY=b.y - a.y
-  deltaDCX=d.x - c.x
-  deltaDCY=d.y - c.y
-  denominator = deltaABX * deltaDCY - deltaABY * deltaDCX
-  if denominator == 0:
-    return None
-  numerator = (a.y - c.y) * deltaDCX - (a.x - c.x) * deltaDCY
-  r = numerator / denominator
-  x = a.x + r * deltaABX
-  y = a.y + r * deltaABY
-  return _Vertex(x,y,0)
+    deltaABX=b.x - a.x
+    deltaABY=b.y - a.y
+    deltaDCX=d.x - c.x
+    deltaDCY=d.y - c.y
+    denominator = deltaABX * deltaDCY - deltaABY * deltaDCX
+    if denominator == 0:
+        return None
+    numerator = (a.y - c.y) * deltaDCX - (a.x - c.x) * deltaDCY
+    r = numerator / denominator
+    x = a.x + r * deltaABX
+    y = a.y + r * deltaABY
+    return _Vertex(x,y,0)
 
 def offsetLine(v1, v2,  offset):
-  v =subtract(v2, v1)
-  v=unitize(v)
-  v=scale(v,offset)
-  t = v.x
-  v.x = -v.y
-  v.y = t
-  v.z=0
-  return _Vertex(add(v1,v),add(v2,v))
+    v =subtract(v2, v1)
+    v=unitize(v)
+    v=scale(v,offset)
+    t = v.x
+    v.x = -v.y
+    v.y = t
+    v.z=0
+    return _Vertex(add(v1,v),add(v2,v))
 
 def offsetPoint(v1,  v2,  v3,  offset1,  offset2):
-  line1= offsetLine(v1, v2, offset1);
-  line2= offsetLine(v2, v3, offset2);
-  return lineLineIntersection(line1.x,line1.y,line2.x,line2.y)
+    line1= offsetLine(v1, v2, offset1);
+    line2= offsetLine(v2, v3, offset2);
+    return lineLineIntersection(line1.x,line1.y,line2.x,line2.y)
 
 def rot2D90(vertex):
     return _Vertex(-vertex.y,vertex.x,vertex.z)
