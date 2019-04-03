7f28426afc908f117dec302e61148cb0e18db692
worbit <bernhard@arch.ethz.ch>
2019-04-03 17:45:25 +0200

    edit __init__ for import *

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from .color import *
+from .core import *
+from .faceUtils import *
+from .factory import *
+from .graph import *
+from .grid import *
+from .io import *
+from .marchingCubes import *
+from .mathUtils import *
+from .polyUtils import *
+from .slicer import *
+from .subdivision import *
+from .vec import *
+
+__all__ = [name for name in dir() if not name.startswith('_')]
