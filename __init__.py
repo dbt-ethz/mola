b08abf55bce40b55239f8d8746616cf85aad55be
worbit <bernhard@arch.ethz.ch>
2020-02-24 21:11:02 +0100

    undo import

--- __init__.py
+++ __init__.py
@@ -11,6 +11,5 @@ from .polyUtils import *
 from .slicer import *
 from .subdivision import *
 from .vec import *
-import renderBabylonJS
 
 __all__ = [name for name in dir() if not name.startswith('_')]
