ff296184d864f5c7d769cb7c8ffac40d7a910922
demsham <desettled@gmail.com>
2020-02-26 14:58:12 +0200

    remove underscores

--- vec.py
+++ vec.py
@@ -7,11 +7,11 @@ __copyright__  = 'Copyright 2019 / Digital Building Technologies DBT / ETH Zuric
 __license__    = 'MIT License'
 __email__      = ['<dbt@arch.ethz.ch>']
 
-import math as _math
-from mola.core import Vertex as _Vertex
+import math as math
+from mola.core import Vertex
 
 def add(v1,v2):
-    return _Vertex(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z)
+    return Vertex(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z)
 
 def angle(v1,v2):
     a=unitize(v1)
@@ -19,26 +19,26 @@ def angle(v1,v2):
     f=dot(a,b)
     if f<-1:f=-1
     if f>1:f=1
-    return _math.acos(f)
+    return math.acos(f)
 
 def angleTriangle(vPrevious,v,vNext):
   #law of cosines
   vvn = distance(v,vNext)
   vvp = distance(vPrevious,v)
   vnvp = distance(vNext,vPrevious)
-  return _math.acos((vvn*vvn + vvp*vvp - vnvp*vnvp) / (2 * vvn * vvp))
+  return math.acos((vvn*vvn + vvp*vvp - vnvp*vnvp) / (2 * vvn * vvp))
 
 def subtract(v1,v2):
-    return _Vertex(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z)
+    return Vertex(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z)
 
 def scale(v,factor):
-    return _Vertex(v.x*factor,v.y*factor,v.z*factor)
+    return Vertex(v.x*factor,v.y*factor,v.z*factor)
 
 def divide(v,factor):
-    return _Vertex(v.x/factor,v.y/factor,v.z/factor)
+    return Vertex(v.x/factor,v.y/factor,v.z/factor)
 
 def length(v):
-    return _math.sqrt(v.x*v.x+v.y*v.y+v.z*v.z)
+    return math.sqrt(v.x*v.x+v.y*v.y+v.z*v.z)
 
 def unitize(v):
     l=length(v)
@@ -46,7 +46,7 @@ def unitize(v):
     return divide(v,l)
 
 def cross(v1,v2):
-    return _Vertex(v1.y * v2.z - v2.y * v1.z, v1.z * v2.x - v2.z * v1.x, v1.x * v2.y - v2.x * v1.y)
+    return Vertex(v1.y * v2.z - v2.y * v1.z, v1.z * v2.x - v2.z * v1.x, v1.x * v2.y - v2.x * v1.y)
 
 def dot(v1,v2):
     return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
@@ -55,13 +55,13 @@ def distance(v1,v2):
     dX=v2.x-v1.x
     dY=v2.y-v1.y
     dZ=v2.z-v1.z
-    return _math.sqrt(dX*dX+dY*dY+dZ*dZ)
+    return math.sqrt(dX*dX+dY*dY+dZ*dZ)
 
 def center(v1,v2):
-    return _Vertex((v1.x+v2.x)/2,(v1.y+v2.y)/2,(v1.z+v2.z)/2)
+    return Vertex((v1.x+v2.x)/2,(v1.y+v2.y)/2,(v1.z+v2.z)/2)
 
 def betweenRel( v1,  v2,  f):
-    return _Vertex((v2.x - v1.x) * f + v1.x, (v2.y - v1.y) * f + v1.y, (v2.z - v1.z) * f + v1.z)
+    return Vertex((v2.x - v1.x) * f + v1.x, (v2.y - v1.y) * f + v1.y, (v2.z - v1.z) * f + v1.z)
 
 def betweenAbs( v1,  v2,  f):
     d = distance(v1,v2)
@@ -79,7 +79,7 @@ def lineLineIntersection(a,b,c,d):
     r = numerator / denominator
     x = a.x + r * deltaABX
     y = a.y + r * deltaABY
-    return _Vertex(x,y,0)
+    return Vertex(x,y,0)
 
 def offsetLine(v1, v2,  offset):
     v =subtract(v2, v1)
@@ -89,7 +89,7 @@ def offsetLine(v1, v2,  offset):
     v.x = -v.y
     v.y = t
     v.z=0
-    return _Vertex(add(v1,v),add(v2,v))
+    return Vertex(add(v1,v),add(v2,v))
 
 def offsetPoint(v1,  v2,  v3,  offset1,  offset2):
     line1= offsetLine(v1, v2, offset1);
@@ -97,4 +97,4 @@ def offsetPoint(v1,  v2,  v3,  offset1,  offset2):
     return lineLineIntersection(line1.x,line1.y,line2.x,line2.y)
 
 def rot2D90(vertex):
-    return _Vertex(-vertex.y,vertex.x,vertex.z)
+    return Vertex(-vertex.y,vertex.x,vertex.z)
