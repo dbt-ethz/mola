4592f22810adbcc5f03b7fc24ca491a09c3487f8
worbit <bernhard@arch.ethz.ch>
2020-03-05 21:20:29 +0100

    face coloring

--- utils_color.py
+++ utils_color.py
@@ -8,6 +8,7 @@ __email__      = ['<dbt@arch.ethz.ch>']
 import colorsys
 from mola import utils_face
 from mola import utils_math
+from math import floor
 
 def color_hue_to_rgb(hue, do_grayscale):
     """
@@ -15,13 +16,13 @@ def color_hue_to_rgb(hue, do_grayscale):
     and returns (r,g,b,1)
     """
     if do_grayscale:
-        return (hue,hue,hue,1)
+        return (hue, hue, hue, 1)
     else:
         hue = utils_math.math_map(hue, 0.0, 1.0, 0.0, 0.8) #limit hue red-red to red-magenta
         col = colorsys.hsv_to_rgb(hue, 1, 1)
         return (col[0], col[1], col[2], 1) # alpha = 100 %
 
-def color_faces_by_function(faces,faceFunction, do_grayscale=False):
+def color_faces_by_function(faces, faceFunction, do_grayscale=False):
     """
     Assigns a color to all the faces by face-function which has to return a float value for a face as argument,
     from smallest (red) to biggest (purple).
@@ -62,7 +63,7 @@ def color_map(values=[], colors=[(1,0,0.5),(0,0.5,1)]):
     values_mapped = [utils_math.math_map(v, value_min, value_max, 0.0, 0.999) for v in values]
     interval = 1.0 / (len(colors) - 1)
     output_colors = []
-    for i,v in enumerate(values_mapped):
+    for v in values_mapped:
         lower_ix = int(floor(v * (len(colors)-1)))
         upper_ix = lower_ix + 1
         rv = (v - (lower_ix * interval)) / interval
@@ -120,35 +121,35 @@ def color_faces_by_curvature(faces):
     Assigns a color to all the faces by curvature (require topological meshinformation),
     from smallest (red) to biggest (purple).
     """
-    color_faces_by_function(faces, utils_face.curvature)
+    color_faces_by_function(faces, utils_face.face_curvature)
 
 def color_faces_by_area(faces):
     """
     Assigns a color to all the faces by area,
     from smallest (red) to biggest (purple).
     """
-    color_faces_by_function(faces, utils_face.area)
+    color_faces_by_function(faces, utils_face.face_area)
 
 def color_faces_by_perimeter(faces):
     """
     Assigns a color to all the faces by perimeter,
     from smallest (red) to biggest (purple).
     """
-    color_faces_by_function(faces, utils_face.perimeter)
+    color_faces_by_function(faces, utils_face.face_perimeter)
 
 def color_faces_by_compactness(faces):
     """
     Assigns a color to all the faces by compactness (area/perimeter),
     from smallest (red) to biggest (purple).
     """
-    color_faces_by_function(faces, utils_face.compactness)
+    color_faces_by_function(faces, utils_face.face_compactness)
 
 def color_faces_by_horizontal_angle(faces):
-    color_faces_by_function(faces, utils_face.horizontal_angle)
+    color_faces_by_function(faces, utils_face.face_horizontal_angle)
 
 def color_faces_by_vertical_angle(faces):
     """
     Assigns a color to all the faces by verticality,
     from smallest (red) to biggest (purple).
     """
-    color_faces_by_function(faces, utils_face.vertical_angle)
+    color_faces_by_function(faces, utils_face.face_vertical_angle)
