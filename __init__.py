5110af3c6ee28ebd86cd0e65f7b451307c073aed
Demetris Shammas <desettled@gmail.com>
2020-03-05 16:16:20 +0200

    init

--- __init__.py
+++ __init__.py
@@ -3,17 +3,21 @@ This is where the module documentation goes
 """
 
 from .color import *
-from .core import *
-from .faceUtils import *
-from .factory import *
+from .core_box import *
+from .core_edge import *
+from .core_face import *
+from .core_mesh import *
+from .core_vertex import *
 from .graph import *
 from .grid import *
 from .io import *
-from .marchingCubes import *
-from .mathUtils import *
+from .mesh_factory import *
+from .mesh_marching_cubes import *
+from .mesh_subdivision import *
 from .polyUtils import *
 from .slicer import *
-from .subdivision import *
-from .vec import *
+from .utils_face import *
+from .utils_math import *
+from .utils_vertex import *
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- polyUtils.py
+++ polyUtils.py
@@ -1,4 +1,4 @@
-from mola import vecUtils
+from mola import utils_vertex
 import math
 
 def subdivideCatmull2D(pts):
@@ -7,8 +7,8 @@ def subdivideCatmull2D(pts):
         a = pts[i]
         newNodes.append(Vertex(a.x,a.y,a.z))
         b = pts[(i + 1) % len(pts)]
-        center = vecUtils.vertex_add(a, b)
-        newNodes.append(vecUtils.vertex_scale(center,0.5))
+        center = utils_vertex.vertex_add(a, b)
+        newNodes.append(utils_vertex.vertex_scale(center,0.5))
     newNodes2 = []
     for i in range(len(newNodes)):
         iPrev = i - 1
@@ -21,31 +21,31 @@ def subdivideCatmull2D(pts):
         b = newNodes[i]
         c = newNodes[iNext]
         average = Vertex(0,0,0)
-        average = vecUtils.vertex_add(average,a)
-        average = vecUtils.vertex_add(average,b)
-        average = vecUtils.vertex_add(average,b)
-        average = vecUtils.vertex_add(average,c)
-        average = vecUtils.vertex_divide(average,4.0)
+        average = utils_vertex.vertex_add(average,a)
+        average = utils_vertex.vertex_add(average,b)
+        average = utils_vertex.vertex_add(average,b)
+        average = utils_vertex.vertex_add(average,c)
+        average = utils_vertex.vertex_divide(average,4.0)
         newNodes2.append(average)
     return newNodes2
 
 def normalEdge2DNonUnified(vprev,v):
-    vec1 = vecUtils.vertex_subtract(v, vprev)
-    return vecUtils.vertex_rotate_2D_90(vec1)
+    vec1 = utils_vertex.vertex_subtract(v, vprev)
+    return utils_vertex.vertex_rotate_2D_90(vec1)
 
 def normalEdge2D(vprev,v):
-    vec1 = vecUtils.vertex_subtract(v, vprev)
-    vec1 = vecUtils.vertex_unitize(vec1)
-    return vecUtils.vertex_rotate_2D_90(vec1)
+    vec1 = utils_vertex.vertex_subtract(v, vprev)
+    vec1 = utils_vertex.vertex_unitize(vec1)
+    return utils_vertex.vertex_rotate_2D_90(vec1)
 
 def normalVertex2D(vprev,v,vnext):
-    vec1 = vecUtils.vertex_subtract(v, vprev)
-    vec1 = vecUtils.vertex_unitize(vec1)
-    vec2 = vecUtils.vertex_subtract(vnext, v)
-    vec2 = vecUtils.vertex_unitize(vec2)
-    n = vecUtils.vertex_add(vec1, vec2)
-    n = vecUtils.vertex_scale(n, 0.5)
-    n = vecUtils.vertex_rotate_2D_90(n)
+    vec1 = utils_vertex.vertex_subtract(v, vprev)
+    vec1 = utils_vertex.vertex_unitize(vec1)
+    vec2 = utils_vertex.vertex_subtract(vnext, v)
+    vec2 = utils_vertex.vertex_unitize(vec2)
+    n = utils_vertex.vertex_add(vec1, vec2)
+    n = utils_vertex.vertex_scale(n, 0.5)
+    n = utils_vertex.vertex_rotate_2D_90(n)
     #t=n.x
     #n.x=-n.y
     #n.y=t
