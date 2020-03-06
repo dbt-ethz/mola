56ab1a673c1c7128cdb57c045a503b6c2e42ec15
Demetris Shammas <desettled@gmail.com>
2020-03-06 16:44:56 +0200

    mesh_factory default center = (0,0,0)

--- mesh_factory.py
+++ mesh_factory.py
@@ -97,16 +97,19 @@ def construct_box(x1,y1,z1,x2,y2,z2):
     mesh.update_topology()
     return mesh
 
-def construct_icosahedron(cx,cy,cz,radius):
+def construct_icosahedron(radius,cx=0,cy=0,cz=0):
     """
     Creates and returns a mesh in the form of an icosahedron.
 
     Arguments:
     ----------
-    cx,cy,cz : float
-        The coordinates of the center point
     radius : float
         The radius of the containing sphere
+    ----------
+    Optional Arguments:
+    ----------
+    cx,cy,cz : float
+        The coordinates of the center point.
     """
     mesh = Mesh()
     phi = (1 + 5 ** 0.5) / 2
@@ -139,16 +142,19 @@ def construct_icosahedron(cx,cy,cz,radius):
     mesh.update_topology()
     return mesh
 
-def construct_dodecahedron(cx,cy,cz,radius):
+def construct_dodecahedron(radius, cx=0,cy=0,cz=0):
     """
     Constructs a dodecaheron mesh.
 
     Arguments:
     ----------
-    cx, cy, cz : float
-        The center point of the dodecaheron
     radius : float
         The radius of the containing sphere
+    ----------
+    Optional Arguments:
+    ----------
+    cx,cy,cz : float
+        The coordinates of the center point.
     """
     mesh = Mesh()
     phi = (1 + 5 ** 0.5) / 2
@@ -199,17 +205,21 @@ def construct_dodecahedron(cx,cy,cz,radius):
     mesh.update_topology()
     return mesh
 
-def construct_tetrahedron(cx,cy,cz,side):
+def construct_tetrahedron(size,cx=0,cy=0,cz=0):
     """
     Constructs a tetrahedron mesh.
 
     Arguments:
     ----------
-    cx, cy, cz : float
-        The center point of the tetrahedron
     side : float
         The edge length of the tetrahedron
+    ----------
+    Optional Arguments:
+    ----------
+    cx,cy,cz : float
+        The coordinates of the center point.
     """
+
     mesh = Mesh()
     coord = 1 / math.sqrt(2)
     mesh.vertices = [Vertex(+1, 0, -coord),
@@ -240,10 +250,13 @@ def construct_torus(ringRadius, tubeRadius, ringN = 16, tubeN = 16):
         the big radius of the axis
     tubeRadius : float
         radius of the the tube along the axis
+    ----------
+    Optional Arguments:
+    ----------
     ringN : int
-        resolution along the ring
+        resolution along the ring (default value = 16)
     tubeN : int
-        resolution along the tube
+        resolution along the tube (default value = 16)
     """
     mesh = Mesh()
     theta = 2 * math.pi / ringN
