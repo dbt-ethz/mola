b6f4a930783dba1b97a9865ae9da3587489e6748
Demetris Shammas <desettled@gmail.com>
2020-03-10 17:47:35 +0200

    .

--- __init__.py
+++ __init__.py
@@ -4,10 +4,10 @@ This is where the module documentation goes
 from .core_box import *
 from .core_edge import *
 from .core_face import *
+from .core_grid import *
 from .core_mesh import *
 from .core_vertex import *
 from .graph import *
-from .grid import *
 from .io import *
 from .mesh_factory import *
 from .mesh_marching_cubes import *
