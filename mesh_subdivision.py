fc81b9c35cd5712e720cedc3e0e71b560e0f67ed
modellstadt <contact@benjamin-dillenburger.com>
2020-12-04 22:23:43 +0100

    bug

--- mesh_subdivision.py
+++ mesh_subdivision.py
@@ -109,7 +109,7 @@ def _calculateVertexPoint(vertex):
             v = utils_vertex.vertex_scale(v,6)
             v = utils_vertex.vertex_add(v,sharpEdges[0].center())
             v = utils_vertex.vertex_add(v,sharpEdges[1].center())
-            v = utils_vertex.vertex_divide(8)
+            v = utils_vertex.vertex_divide(v,8)
         # sharp corner
         elif len(sharpEdges)>2:
             v = Vertex(vertex.x, vertex.y, vertex.z)
