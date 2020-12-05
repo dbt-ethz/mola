bb92bfe664b5cf8ca3a7a122ac856f93caa893c0
modellstadt <contact@benjamin-dillenburger.com>
2020-12-05 11:58:10 +0100

    test

--- mesh_subdivision.py
+++ mesh_subdivision.py
@@ -141,7 +141,7 @@ def _calculateVertexPoint(vertex):
         sharpEdges=[]
         vSharpness=0
         for edge in vertex.edges:
-            if edge.sharpness > 0:
+            if edge.sharpness >= 1:
                 sharpEdges.append(edge)
                 vSharpness += edge.sharpness
 
