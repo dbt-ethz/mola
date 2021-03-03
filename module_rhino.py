83b9f09213ce4ed4c0676e98de045263df6a5ae6
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2021-03-03 14:14:54 +0100

    added return to display_faces() to work both in rhino + grasshopper

--- module_rhino.py
+++ module_rhino.py
@@ -36,4 +36,6 @@ def display_faces(faces):
             vertices.append((v.x,v.y,v.z))
             vertexColors.append((f.color[0]*255,f.color[1]*255,f.color[2]*255))
         facesIndices.append(faceIndices)
-    rs.AddMesh(vertices,facesIndices,None,None,vertexColors)
+    a = rs.AddMesh(vertices,facesIndices,None,None,vertexColors)
+
+    return a
